e133d36f30a2f54b6ac0e2f95c34422c75186afd
Rohit Raja Pahan <rohit.raja56@gmail.com>
2017-09-19 02:22:07 +0530

    Prod invironment

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-
+var port = process.env.PORT || 8000
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log('Example app listening on port 8000!')
 })
